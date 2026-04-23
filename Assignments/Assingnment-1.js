ca709513f3643fd67667b50a6b10e5a533346e18
Farhan Musharraf <144747565+FarhanMusharraf@users.noreply.github.com>
2026-04-23 12:23:25 +0000

    Find minimum number learned

--- Assignments/Assingnment-1.js
+++ Assignments/Assingnment-1.js
@@ -1,16 +1,38 @@
-//Print Element
-// let arr = [5, 10, 15];
 
+
+//Print elements
+// let arr = [5,10,15];
 // for (let i = 0; i < arr.length; i++) {
 //     let print = arr[i]
-//     console.log (`array @ index ${i} is equal to: ${print} `)
-//     }
+//     console.log (print)
 
-//sum of numbers
-let arr1 = [5, 10, 15, 20, 25, 30];
-let sum = 0;
-for (let i = 0; i < arr1.length; i++) {
-    let index = arr1[i];
-    sum = index + sum
+// }
+
+//Sum of numbers
+// let arr = [5,10,15];
+// let sum = 0
+// for (let i = 0; i < arr.length; i++) {
+//     let element = arr[i];
+//     sum+= element
+//     console.log (sum) //output 5 15 30
+// }
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     let element = arr[i];
+//     sum+= element
+// }
+// console.log (sum) //output 30
+
+//Find the minimum value
+let arr = [5, 10, 15];
+let minV = arr[0] //arr[?] This is called bracket notation. It is how you access one specific item from an array. The number you put inside the brackets is called the index — it tells JavaScript which position to go to.
+for (let i = 1; i < arr.length; i++) {
+
+    if (arr[i] < minV) {
+
+        minV = arr[i];
+    }
+
 }
-console.log(`the sum of arr1 is = ${sum}`);
+console.log(`Minimum number is 5 : ${minV} `)
